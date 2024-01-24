0084c31e48b0649da306730d0f812c0e33352433
lihka1202 <mnakhilwork@gmail.com>
2024-01-24 17:55:25 +0800

    feat(setup) : linters and formatters set up

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    webextensions: true,
+  },
+  plugins: ['prettier'],
+  extends: 'airbnb-base',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-use-before-define': 'off',
+  },
+};
