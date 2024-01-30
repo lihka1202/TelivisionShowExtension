ea9548cc6d94d69ea389cd058b922aac4a6e9a10
lihka1202 <mnakhilwork@gmail.com>
2024-01-30 22:39:26 +0800

    feat(tabs): add tabs and search api

--- background.js
+++ background.js
@@ -7,20 +7,10 @@ chrome.runtime.onInstalled.addListener(() => {
 
   chrome.contextMenus.onClicked.addListener((event) => {
     console.log(event);
-  });
-
-  chrome.contextMenus.create({
-    title: 'Test this context',
-    id: 'contextMenu2',
-    parentId: 'contextMenu1',
-    contexts: ['page', 'selection'],
-  });
-
-  chrome.contextMenus.create({
-    title: 'Test this context',
-    id: 'contextMenu3',
-    parentId: 'contextMenu1',
-    contexts: ['page', 'selection'],
+    chrome.tabs.create({
+      active: true,
+      url: 'https://www.google.com',
+    });
   });
 });
 
